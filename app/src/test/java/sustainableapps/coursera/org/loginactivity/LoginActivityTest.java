cfabebbc8a5eef68475da9dba46c1ed64244738b
Anastasia Shatova <a_tasja@mail.ru>
2017-12-08 12:02:39 +0300

    Added examples of Instrumental tests and tests with Mockito. Updated build.gradle for all these tests. Added Espresso tests and helper class for testing Toast messages.

--- app/src/test/java/sustainableapps/coursera/org/loginactivity/LoginActivityTest.java
+++ app/src/test/java/sustainableapps/coursera/org/loginactivity/LoginActivityTest.java
@@ -10,57 +10,61 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class LoginActivityTest {
+
     private LoginActivity loginActivity = new LoginActivity();
 
     @Test
-    public void validAtSign() throws Exception{
+    public void validAtSign() throws Exception {
         assertEquals(true, loginActivity.isAtSignInEmailAddress("a_tasja@mail.ru"));
     }
 
     @Test
-    public void invalidAtSign() throws Exception{
+    public void invalidAtSign() throws Exception {
         assertFalse(loginActivity.isAtSignInEmailAddress("a_tasja_mail.ru"));
     }
 
     @Test
-    public void validStartOfEmail() throws Exception{
+    public void validStartOfEmail() throws Exception {
         assertTrue(loginActivity.isOneCharBeforeAt("a_tasja@mail.ru"));
     }
 
     @Test
-    public void invalidStartOfEmail() throws Exception{
+    public void invalidStartOfEmail() throws Exception {
         assertFalse(loginActivity.isOneCharBeforeAt("@mail.ru"));
     }
 
     @Test
-    public void validEndOfEmail() throws Exception{
+    public void validEndOfEmail() throws Exception {
         assertTrue(loginActivity.isThreeCharAfterAtSign("a_tasja@mail.ru"));
     }
 
     @Test
-    public void endOfEmailWithoutDot() throws Exception{
+    public void endOfEmailWithoutDot() throws Exception {
         assertFalse(loginActivity.isThreeCharAfterAtSign("a_tasja@mail_ru"));
     }
 
     @Test
-    public void endOfEmailLengthLess3() throws Exception{
+    public void endOfEmailLengthLess3() throws Exception {
         assertFalse(loginActivity.isThreeCharAfterAtSign("a@to"));
     }
 
     @Test
-    public void validPassLength() throws Exception{
+    public void validPassLength() throws Exception {
         assertTrue(loginActivity.passwordLength("19withlove76"));
     }
+
     @Test
-    public void invalidPassLength() throws Exception{
+    public void invalidPassLength() throws Exception {
         assertFalse(loginActivity.passwordLength("12we"));
     }
+
     @Test
-    public void passNotAllSpaces() throws Exception{
-        assertFalse(loginActivity.isPassAllSpaces("123 vbg5"));
+    public void passNotAllSpaces() throws Exception {
+        assertFalse(loginActivity.isPassAllSpaces("12  456789"));
     }
+
     @Test
-    public void passAllSpaces() throws Exception{
-        assertTrue( loginActivity.isPassAllSpaces("    "));
+    public void passAllSpaces() throws Exception {
+        assertTrue(loginActivity.isPassAllSpaces("    "));
     }
 }
